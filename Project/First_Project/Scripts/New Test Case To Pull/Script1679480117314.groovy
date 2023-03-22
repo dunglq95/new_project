1a63d0a61c6f7b5fcf386900db4e2994d780c1c0
dunglq95 <dunglq@firegroup.io>
2023-03-22 17:43:52 +0700

    Change

--- Project/First_Project/Scripts/New Test Case To Pull/Script1679480117314.groovy
+++ Project/First_Project/Scripts/New Test Case To Pull/Script1679480117314.groovy
@@ -17,3 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+//MAKE CHANGE 1
+//MAKE CHANGE 2
